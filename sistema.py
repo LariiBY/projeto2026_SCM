971560fe28e89304aa413e30c0ef5a833c5934b8
Luiz Gustavo <larycristinaaraujo20@gmail.com>
2026-09-12 15:40:34 -0300

    Adicionar sistema.py

---
+++ sistema.py
@@ -0,0 +1,12 @@
+print("Sistema de Controle de Tarefas")
+
+tarefas = [
+    "Desenvolver funcionalidade de cadastro",
+    "Realizar testes do sistema",
+    "Atualizar documentação"
+]
+
+print("\nTarefas da equipe:")
+
+for tarefa in tarefas:
+    print("-", tarefa)
